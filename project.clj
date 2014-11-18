b595d64444d5715622734d5d4f1fdbad9d84d6b3
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-11-18 12:42:53 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir-exception "0.2.2"
+(defproject noir-exception "0.2.3"
  :description "middleware for displaying Noir style exceptions in the browser"
  :url "https://github.com/yogthos/noir-exception"
  :license {:name "Eclipse Public License"
